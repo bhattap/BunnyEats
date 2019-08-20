1fe933c0fc407fc5cc62b1ff3ea3ab127ef1124b
Puja Bhattacharjee <stevieisme3290@gmail.com>
2019-08-19 20:08:41 -0400

    Implemented method to get longitude and latitude from the user's last location. Added location permissions.

--- app/src/main/java/com/example/bunnyeats/WeatherResult.java
+++ app/src/main/java/com/example/bunnyeats/WeatherResult.java
@@ -1,18 +1,36 @@
 package com.example.bunnyeats;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class WeatherResult extends AppCompatActivity {
 
     private String weatherName;
     private RelativeLayout imageLayout;
     private TextView resultTextView;
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private double longitude;
+    private double latitude;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +42,10 @@ public class WeatherResult extends AppCompatActivity {
         imageLayout = findViewById(R.id.imageLayout);
         resultTextView = findViewById(R.id.resultTextView);
         setBackground();
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        permissionForLocation();
+
     }
 
     private void setBackground(){
@@ -56,7 +78,48 @@ public class WeatherResult extends AppCompatActivity {
         }
     }
 
-    private void createList(){
+    private void permissionForLocation(){
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                    2000);
+        } else{
+            getLocation();
+        }
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 2000){
+            if (grantResults.length > 0 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getLocation();
+            }
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private void getLocation(){
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            latitude = location.getLatitude();
+                            longitude = location.getLongitude();
+                            createList(longitude, latitude);
+                        }
+                    }
+                });
+    }
+
+    private void createList(double longitude, double latitude){
+        Toast.makeText(WeatherResult.this, longitude+", " + latitude, Toast.LENGTH_LONG).show();
     }
 }
